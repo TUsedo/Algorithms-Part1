5b53f5525c1cab6d39df8d3f386b652cf0db7e97
Tused0 <gorasiajayesh@gmail.com>
2015-10-24 11:42:22 -0400

    Add Randomized queue

--- Queues/src/RandomizedQueue.java
+++ Queues/src/RandomizedQueue.java
@@ -1,4 +1,12 @@
+import java.util.Iterator;
 
-public class RandomizedQueue {
+public class RandomizedQueue<Item> implements Iterable<Item> {
 
+  public Iterator<Item> iterator() {
+  
+    return null;
+  }
+  public static void main(String[] args) {
+    
+  }
 }
